1ce65ff75dafa73aefda9207fce5fbe72e24c85f
IhabProjects <eureka.lucis@gmail.com>
2025-03-24 15:13:21 +0000

    Routing Start

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+
+// sign up
+export const signup = (req, res) => {
+    res.send("signup route");
+  };
+
+  // log in
+export const login = (req, res) => {
+    res.send("login route");
+  };
+
+  // log out
+export const logout = (req, res) => {
+    res.send("logout route");
+  };
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+// sign up
+router.post("/signup", signup);
+
+// log in
+router.post("/login",login);
+
+// log out
+router.post("/logout",logout);
+export default router;
