c5f6d4830f9d5c6f165900d10f8c36f534b0a00b
IhabProjects <eureka.lucis@gmail.com>
2025-03-24 22:03:02 +0000

    add updating pfp

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 // Controlling Routes
 
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -112,5 +113,19 @@ export const logout = (req, res) => {
 
 //Updating Profile
 export const updateProfile = async (req, res) => {
-    
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id
+
+        if(!profilePic) {
+            return res.status(400).json({message:"Profile Pic Is required"})
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("error in updated profile:", error)
+        res.status(500).json({message: "Internal Server Error"})
+    }
 }
