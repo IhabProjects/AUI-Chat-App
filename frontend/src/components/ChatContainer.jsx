05cb933997533013b5412705825f513925c7c45f
IhabProjects <eureka.lucis@gmail.com>
2025-03-27 15:36:07 +0000

    add: ChatHeader

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useEffect } from 'react'
+
+import { useChatStore } from '../store/useChatStore'
+//importing the ChatHeader and MessageInput components
+import ChatHeader from './ChatHeader'
+import MessageInput from './MessageInput'
 
 function ChatContainer() {
-  return (
-    <div>ChatContainer</div>
+    const {messages, getMessages, isMessagesLoading, selectedUser}=useChatStore()
+
+
+    useEffect(() => {
+        getMessages(selectedUser._id)
+
+      }, [selectedUser._id, getMessages])
+    if (isMessagesLoading) return <div>Loading...</div>
+
+    return (
+    <div className='flex-1 flex flex-col overflow-y-auto'>
+        <ChatHeader />
+        <p>messages...</p>
+
+        <MessageInput />
+    </div>
   )
 }
 
