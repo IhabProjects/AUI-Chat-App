a99097b3771fe5baa75187a067843d0e23fe8c02
IhabProjects <eureka.lucis@gmail.com>
2025-03-27 14:55:31 +0000

    working: chat interface

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import { useChatStore } from "../store/useChatStore";
+
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
+  const { selectedUser } = useChatStore();
+
   return (
-    <main className="min-h-screen pt-20 px-4 bg-base-200">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold">Home Page</h1>
-        <p className="text-base-content/70">Welcome to AUI Connect!</p>
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 };
 export default HomePage;
