fed94542f43f6a30b1b063dd47232946fc4980a5
IhabProjects <eureka.lucis@gmail.com>
2025-03-27 15:40:43 +0000

    add: MessageSkeleton for loading

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,6 +5,7 @@ import { useChatStore } from '../store/useChatStore'
 //importing the ChatHeader and MessageInput components
 import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
+import MessageSkeleton from './skeletons/MessageSkeleton'
 
 function ChatContainer() {
     const {messages, getMessages, isMessagesLoading, selectedUser}=useChatStore()
@@ -14,7 +15,13 @@ function ChatContainer() {
         getMessages(selectedUser._id)
 
       }, [selectedUser._id, getMessages])
-    if (isMessagesLoading) return <div>Loading...</div>
+    if (isMessagesLoading) return (
+        <div className='flex-1 flex flex-col overflow-auto'>
+            <ChatHeader />
+            <MessageSkeleton />
+            <MessageInput />
+        </div>
+    )
 
     return (
     <div className='flex-1 flex flex-col overflow-y-auto'>
